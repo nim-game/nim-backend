67324f71c197342f9aacb983a3e2e61ca75ca85d
Jakob Fischer <jako.fischer@outlook.de>
2025-09-02 09:28:18 +0200

    add:master

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import type { Socket } from 'net';
 import type { BoxState, ServerMessage, ClientMessage } from './types';
 
 
-const PORT = Number(process.env.PORT || 8080);
-
+const PORT = 80
 
 const app = express();
 const main = app.listen(PORT, () => console.log(`[cakeboxes] http/ws on :${PORT}`));
